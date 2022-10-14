83d5096d4707ae3d617dce487c671e9a21537a00
Sebastian Garcia <sebas_23_1999@outlook.com>
2022-10-14 18:21:04 -0500

    products and categories model created and fetched schemas from remote

---
+++ bsale_challenge_api/src/index.js
@@ -0,0 +1,24 @@
+import { app } from "./app.js";
+import { sequelize } from "./database.js";
+import { Category } from "./models/category.js";
+import { Product } from "./models/product.js";
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    const schemas = await sequelize.showAllSchemas()
+    console.log(schemas)
+    const products = await Product.findAll({})
+    const categories = await Category.findAll({})
+    console.log(categories)
+    // await sequelize.sync({
+    //   alter: false
+    // });
+    console.log("Connection with database has been established");
+    app.listen(5000);
+    console.log("server is listening on port ", 5000);
+    // const products = await Product.findAll({});
+  } catch (error) {
+    console.error("Unable to connect to database", error);
+  }
+};
+main();
